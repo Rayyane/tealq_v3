aed133ab9f5cede4b9705d3fc6e66db2c419aec0
Rayyane <akibmr9@gmail.com>
2023-03-20 10:47:49 +0600

    footer update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
 AOS.init();
+
+const navEl = document.querySelector('.navbar');
+window.addEventListener('scroll', function() {
+	if (window.innerWidth > 500 && window.scrollY >= 56) {
+		navEl.classList.add('navbar-scrolled');
+	}
+	else if (window.innerWidth > 500 && window.scrollY < 56){
+		navEl.classList.remove('navbar-scrolled');
+	}
+	else if (window.innerWidth <= 500 && window.scrollY >= 2) {
+		navEl.classList.add('navbar-scrolled');
+	}
+	else if (window.innerWidth <= 500 && window.scrollY < 2) {
+		navEl.classList.remove('navbar-scrolled');
+	}
+});
